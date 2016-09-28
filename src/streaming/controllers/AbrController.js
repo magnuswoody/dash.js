ef385c85d242103be9f6ca2a2d3560d192c9eac1
Jonty Usborne <jonty.usborne@bbc.co.uk>
2016-09-28 15:59:45 +0100

    remove console.log accidentally left from debugging

--- src/streaming/controllers/AbrController.js
+++ src/streaming/controllers/AbrController.js
@@ -481,7 +481,6 @@ function AbrController() {
             var pixelRatio = hasPixelRatio ? window.devicePixelRatio : 1;
             elementWidth = element.clientWidth * pixelRatio;
             elementHeight = element.clientHeight * pixelRatio;
-            console.log('Portal resize: ' + elementWidth + ' ' + elementHeight);
         }
     }
 
