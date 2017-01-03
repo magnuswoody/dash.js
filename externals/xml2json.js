3591117cb34a8270619672a33206adf435f8d8eb
robertbryer <robert.bryer@bbc.co.uk>
2017-01-03 12:49:29 +0000

    Don't use requireJS for xml2json

--- externals/xml2json.js
+++ externals/xml2json.js
@@ -25,11 +25,11 @@
 */
 
 (function (root, factory) {
-     if (typeof define === "function" && define.amd) {
-         define([], factory);
-     } else if (typeof exports === "object") {
+     if (typeof exports === "object") {
          module.exports = factory();
-     } else {
+     } else if (typeof define === "function" && define.amd) {
+         define([], factory);
+     }  else {
          root.X2JS = factory();
      }
  }(this, function () {
